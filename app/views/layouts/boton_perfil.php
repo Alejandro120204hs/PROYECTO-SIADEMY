9392ccd8bd48eb00e160a862f5a1aeb87e0307f3
Julian Amaya <julianamaya354@gmail.com>
2026-02-03 14:07:18 -0500

    arreglando la parte de el header donde muestra datos del usuario

--- app/views/layouts/boton_perfil.php
+++ app/views/layouts/boton_perfil.php
@@ -1,4 +1,12 @@
-<button class="btn" title="Notificaciones"><i class="ri-notification-3-line"></i></button>
-<button class="btn" title="Configuración"><i class="ri-settings-3-line"></i></button>
-<div class="avatar" ><a href="<?= BASE_URL ?>/dashboard-perfil"><img src="<?= BASE_URL ?>/public/uploads/usuarios/<?= $usuario['foto'] ?>" 
-         alt="foto" width="40px" height="40px" style="border-radius: 50%;"></a></div>
+<div class="user-info">
+  <div class="user-details">
+    <span class="user-name"><?= $usuario['nombres'] ?></span>
+    <span class="user-role"><?= $usuario['rol'] ?></span>
+  </div>
+</div>
+<div class="avatar">
+  <a href="<?= BASE_URL ?>/dashboard-perfil">
+    <img src="<?= BASE_URL ?>/public/uploads/usuarios/<?= $usuario['foto'] ?>" 
+         alt="foto" width="40px" height="40px" style="border-radius: 50%;">
+  </a>
+</div>
